c07158422dbfd7d74f8e574b4392f3b8d6b07d2a
MJMMiller <mateomiller1605@gmail.com>
2024-03-15 16:25:19 -0600

    V1

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,39 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  temperatura: number;
+
+  constructor() {
+    this.temperatura=0;
+  }
+
+  ngOnInit(){
+    const range = document.getElementById('range') as HTMLInputElement;
+    const circle = document.getElementById('circle');
+
+    if(range && circle){
+      range.addEventListener('input',()=>{
+        const value=parseInt(range.value);
+        const angle =(value/100)*360;
+        circle.style.transform=`rotate(${angle}deg) Translate(-50%, -50%)`;
+      });
+    }else{
+      console.error("No se encontro el elemento pedido")
+    }
+  }
+
+  getColor(){
+    if(this.temperatura>=0 && this.temperatura<= 30){
+      return 'primary'; // Azul
+    }else if(this.temperatura>31 && this.temperatura<= 59){
+      return 'success'; // Verde
+    }else if(this.temperatura>=60 && this.temperatura<= 80){
+      return 'warning'; // Amarillo
+    }else if(this.temperatura>=81 && this.temperatura<= 100){
+      return 'danger'; // Rojo
+    }else{
+      return 'primary';
+    }
+  }
 
 }
